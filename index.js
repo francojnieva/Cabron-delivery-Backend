0835b3d4ee635d1e63475649e418696b6024e1c0
francojnieva <franco97nieva@hotmail.com>
2024-07-02 17:11:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const PORT = 6000
+
+app.listen(PORT, () => {
+    console.log(`Server OK on port ${PORT}`)
+})
+
+
+
