c784732e13d9872940a756f58c22d2c6b9b10bc8
francojnieva <franco97nieva@hotmail.com>
2024-08-16 20:11:06 -0300

    add middleware to edit user route

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
-const { registerUser, getAllUsers, getOneUser, loginUser, deleteUser, auth, editUser } = require('../controllers/users.controllers')
+const { registerUser, getAllUsers, getOneUser, loginUser, deleteUser, editUser } = require('../controllers/users.controllers')
 const authUser = require('../middleware/auth.middleware')
 const router = Router()
 
 router.post('/api/registro', registerUser)
 router.post('/api/iniciar-sesion', loginUser)
-router.put('/api/editar-usuario', editUser)
+router.put('/api/editar-usuario', authUser, editUser)
 router.get('/api/usuarios', authUser, getAllUsers)
 router.get('/api/usuario', authUser, getOneUser)
 router.delete('/api/usuario', authUser, deleteUser)
